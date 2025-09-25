aee75280efe40cf372a77cde626d82e139d428f6
Ayushmaan Mishra <ayush8171wiz@gmail.cpm>
2025-09-25 21:51:30 +0530

    f commit

--- src/routes/Allroutes.jsx
+++ src/routes/Allroutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
 
-const Allroutes = () => {
+const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
@@ -10,4 +10,4 @@ const Allroutes = () => {
   );
 };
 
-export default Allroutes;
+export default AllRoutes;
