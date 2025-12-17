660dbf48dbfe663049117e6a6a8a74b292925fad
Ayushmaan Mishra <ayush8171wiz@gmail.cpm>
2025-12-17 19:47:49 +0530

    f commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import AllRoutes from "./routes/AllRoutes.jsx";
+import ScrollToTop from "./routes/ScrollToTop.jsx";
 
 function App() {
   return (
     <div className="app">
+      <ScrollToTop/>
       <AllRoutes />
     </div>
   );
